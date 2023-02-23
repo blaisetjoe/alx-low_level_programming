fbbf15660ddbbf9fd8bc4bb517fe142120223027
Blaise Igiraneza <blaiseigiraneza@gmail.com>
2023-02-23 12:32:05 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_abs - computes the absolute of an intenger
+*@n: the intenger to be computed
+*
+* Return: the absolute value of the intenger.
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}

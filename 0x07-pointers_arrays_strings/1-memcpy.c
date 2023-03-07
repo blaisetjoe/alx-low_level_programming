79f04333d661678e023c0d9d8564e05db89c71d9
Blaise Igiraneza <blaiseigiraneza@gmail.com>
2023-03-07 11:20:30 -0800

    initial commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset: fill the first @n bytes of the memory area pointed to
+ * by @s with the constant byte @b
+ * @n: bytes of the memory area pointed to
+ * @s: with the constant byte @b
+ * @b: memory pointer
+ *
+ * Return: a pointer to the memory area @s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}

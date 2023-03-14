c30b29c5cd202f11c1131802e1f2de148452d92d
Blaise Igiraneza <blaiseigiraneza@gmail.com>
2023-03-14 12:00:41 -0700

    me don tire o

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	for (i = 0; s1[1] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		ar[i] = s1[i];
 
 	for (j = i, k = 0; s2[k] != '\0'; j++, k++)

88f65f6811194c1e9c8d22dbd6cebf037b5cc8fc
Blaise Igiraneza <blaiseigiraneza@gmail.com>
2023-03-20 10:41:50 -0700

    fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,10 +15,10 @@ if (d == NULL)
 if ((*d).name == NULL)
 	printf("Name: (nil)\n");
 else
+{
 	printf("Name: %s\n", (*d).name);
-
-
 	printf("Age: %f\n", (*d).age);
+}
 
 if ((*d).owner == NULL)
 	printf("Owner: (nil)\n");
